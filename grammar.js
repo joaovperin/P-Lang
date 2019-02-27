995a82e378df98d9fc0cfe99546bc9605d2e985e
Perin <perin@rech.com.br>
2019-02-27 12:48:42 -0300

    Created hello world project, based on the tracerly documentation (https://github.com/v21/tracery)

---
+++ grammar.js
@@ -0,0 +1,8 @@
+/**
+ * Returns the grammar rules
+ */
+module.exports = Object.freeze({
+    'animal': ['panda', 'fox', 'capybara', 'iguana'],
+    'emotion': ['sad', 'happy', 'angry', 'jealous'],
+    'origin': ['I am #emotion.a# #animal#.'],
+});
---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * Main entry-point - Executes all the program logic within the grammar rules
+ */
+const tracery = require('tracery-grammar');
+const grammarSource = require('./grammar');
+
+module.exports = (() => {
+
+    var grammar = tracery.createGrammar(grammarSource);
+
+    grammar.addModifiers(tracery.baseEngModifiers);
+    console.log(grammar.flatten('#origin#'));
+
+})();
